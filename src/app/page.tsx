9c4e72d415217b73f8300955478dc3d41c6080e6
Mahendra Bairwa <myselfmk061@gmail.com>
2025-07-23 16:46:55 +0000

    आज का पंचांग upperest side on Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -78,12 +78,15 @@ export default function Home() {
           </p>
         </header>
 
+        <div className="max-w-md mx-auto w-full mb-8">
+            <PanchangDetailCard panchang={selectedPanchang} isLoading={isLoading} />
+        </div>
+
         <div className="grid grid-cols-1 xl:grid-cols-3 gap-8 items-start">
           <div className="xl:col-span-2">
             <FestivalSearch onDateSelect={setSelectedDate} />
           </div>
           <div className="xl:col-span-1 flex flex-col gap-8">
-            <PanchangDetailCard panchang={selectedPanchang} isLoading={isLoading} />
             <SunTimesCard panchang={selectedPanchang} isLoading={isLoading} />
             <ChaughadiyaCard title="दिन का चौघड़िया" />
             <ChaughadiyaCard title="रात का चौघड़िया" />
