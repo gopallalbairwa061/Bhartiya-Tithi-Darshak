c2f9e6625896f423bccb416459852b38e77fd968
Mahendra Bairwa <myselfmk061@gmail.com>
2025-07-23 16:24:24 +0000

    Please remove loding screen in text

--- src/components/loading-screen.tsx
+++ src/components/loading-screen.tsx
@@ -6,7 +6,6 @@ export function LoadingScreen() {
       <div className="animate-pulse">
         <LogoIcon className="h-24 w-24" />
       </div>
-      <p className="text-xl text-primary font-semibold mt-4">लोड हो रहा है...</p>
     </div>
   );
 }
