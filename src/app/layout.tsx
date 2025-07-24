89f6065583dde22d19946592e412081fde51346b
Mahendra Bairwa <myselfmk061@gmail.com>
2025-07-24 06:47:54 +0000

    <script type="module">   // Import the functions you need from the SDKs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+
 import type { Metadata } from 'next';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
@@ -33,6 +34,34 @@ export default function RootLayout({
           {children}
           <Toaster />
         </ThemeProvider>
+        <script
+          type="module"
+          dangerouslySetInnerHTML={{
+            __html: `
+              // Import the functions you need from the SDKs you need
+              import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+              import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-analytics.js";
+              // TODO: Add SDKs for Firebase products that you want to use
+              // https://firebase.google.com/docs/web/setup#available-libraries
+
+              // Your web app's Firebase configuration
+              // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+              const firebaseConfig = {
+                apiKey: "AIzaSyDygAFpVjw0HyvWKbt8WPP-oZNabIz5ob0",
+                authDomain: "bhartiya-tithi-darshak.firebaseapp.com",
+                projectId: "bhartiya-tithi-darshak",
+                storageBucket: "bhartiya-tithi-darshak.firebasestorage.app",
+                messagingSenderId: "1052160218081",
+                appId: "1:1052160218081:web:f234e1dc5b3655e56378a0",
+                measurementId: "G-FQ6SF66JXM"
+              };
+
+              // Initialize Firebase
+              const app = initializeApp(firebaseConfig);
+              const analytics = getAnalytics(app);
+            `,
+          }}
+        />
       </body>
     </html>
   );
